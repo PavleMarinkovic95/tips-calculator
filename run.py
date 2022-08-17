417d119e171d72edbfe7363c3f8dd4156c6af9d5
Pavle <105302576+PavleMarinkovic95@users.noreply.github.com>
2022-08-17 18:21:17 +0200

    Add a while true loop to ask for data until valid

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
-from lib2to3.pgen2.token import SLASHEQUAL
 import gspread
 from google.oauth2.service_account import Credentials
-
+    
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -16,27 +15,34 @@ SHEET = GSPREAD_CLIENT.open('tips_spreadsheet')
 
 def get_tips_data():
     # Gets tips data from the user for the current week
-    print("Please enter your tips from monday to friday in dollars \n")
-    print("Your data should be 5 numbers, separated by commas")
-    print("Example: 9, 5.5, 10.2, 4, 5\n")
-    
-    data_str = input("Enter your tips here: ")
-    
-    tips_data = data_str.split(",")
-    validate_tips(tips_data)
+    while True:
+        print("Please enter your tips from monday to friday in dollars \n")
+        print("Your data should be 5 numbers, separated by commas")
+        print("Example: 9, 5.5, 10.2, 4, 5\n")
+        
+        data_str = input("Enter your tips here: ")
+        
+        tips_data = data_str.split(",")
+        
+        if validate_tips(tips_data):
+            print ("Data is valid \n")
+            break
+
+    return tips_data
     
 def validate_tips(values):
     # Inside the try, we convert the the string values to floats
     # If the value cannot be converted, throw an Error, 
     # If there are not 5 values for 5 days of the week, also throw Error
     try:
-        [float(value) for value in values]
+        [float(value) for value in values]1
         if len(values) != 5:
             raise ValueError(
                 f"Exatcly five values are required, you provided {len(values)}"
             )  
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")  
-    print(values)
+        return False
+    return True
 
 get_tips_data()
